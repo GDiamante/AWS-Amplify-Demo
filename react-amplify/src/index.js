7b1a494dfa5ac3224217759d24f841443c295f9c
GDiamante <gdiamante123@gmail.com>
2020-12-01 14:42:34 -0500

    Amplify Changes

--- react-amplify/src/index.js
+++ react-amplify/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+Amplify.configure(awsconfig);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
